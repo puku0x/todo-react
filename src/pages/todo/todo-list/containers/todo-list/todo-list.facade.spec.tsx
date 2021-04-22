8d28005b8b27d8f2d815b739ca7c5ca2a16f93af
puku0x <puku0x@gmail.com>
2021-04-22 23:46:04 +0900

    feat(todo-list): add todo list facade

---
+++ src/pages/todo/todo-list/containers/todo-list/todo-list.facade.spec.tsx
@@ -0,0 +1,48 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { FunctionComponent } from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+
+import { fetchAllTodos } from '../../../../../store/todo';
+import { useTodoListFacade } from './todo-list.facade';
+
+const mockDispatch = jest.fn().mockResolvedValue({});
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+  useSelector: () => jest.fn(),
+}));
+
+jest.mock('../../../../../store/todo', () => ({
+  ...jest.requireActual('../../../../../store/todo'),
+  fetchAllTodos: jest.fn(),
+}));
+
+const mockStore = configureStore();
+const store = mockStore();
+
+const wrapper: FunctionComponent = ({ children }) => (
+  <MemoryRouter>
+    <Provider store={store}>{children}</Provider>
+  </MemoryRouter>
+);
+
+describe('useTodoListFacade', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should dispatch fetchAllTodos', async () => {
+    const offset = 0;
+    const limit = 10;
+    const { result } = renderHook(() => useTodoListFacade({ offset, limit }), {
+      wrapper,
+    });
+    const { fetchAll } = result.current;
+    fetchAll({ offset, limit });
+
+    expect(mockDispatch).toHaveBeenCalled();
+    expect(fetchAllTodos).toHaveBeenCalledWith({ offset, limit });
+  });
+});
---
+++ src/pages/todo/todo-list/containers/todo-list/todo-list.facade.ts
@@ -0,0 +1,56 @@
+import { AnyAction, ThunkDispatch, unwrapResult } from '@reduxjs/toolkit';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import {
+  fetchAllTodos,
+  isFetchingSelector,
+  todosSelector,
+} from '../../../../../store/todo';
+
+export const useTodoListFacade = (arg: { offset?: number; limit?: number }) => {
+  const { offset, limit } = arg;
+  const history = useHistory();
+  const location = useLocation();
+  const dispatch = useDispatch<ThunkDispatch<unknown, undefined, AnyAction>>();
+  const isFetching = useSelector(isFetchingSelector);
+  const todos = useSelector(todosSelector);
+
+  const fetchAll = useCallback(
+    (arg: { offset?: number; limit?: number }) => {
+      return dispatch(fetchAllTodos(arg)).then(unwrapResult);
+    },
+    [dispatch]
+  );
+
+  const changeOffset = useCallback(
+    (offset: number) => {
+      const params = new URLSearchParams(location.search);
+      params.set('offset', `${offset}`);
+      history.push(`/todos?${params}`);
+    },
+    [history, location.search]
+  );
+
+  const changeLimit = useCallback(
+    (limit: number) => {
+      const params = new URLSearchParams(location.search);
+      params.set('limit', `${limit}`);
+      history.push(`/todos?${params}`);
+    },
+    [history, location.search]
+  );
+
+  useEffect(() => {
+    fetchAll({ offset, limit });
+  }, [offset, limit, fetchAll]);
+
+  return {
+    isFetching,
+    todos,
+    changeOffset,
+    changeLimit,
+    fetchAll,
+  } as const;
+};
