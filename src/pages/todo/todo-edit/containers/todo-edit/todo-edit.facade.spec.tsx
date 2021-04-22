b636682da84e54492f7a3be4951632e8c465016c
puku0x <puku0x@gmail.com>
2021-04-22 23:29:56 +0900

    feat(todo-edit): add todo edit facade

---
+++ src/pages/todo/todo-edit/containers/todo-edit/todo-edit.facade.spec.tsx
@@ -0,0 +1,69 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { FunctionComponent } from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+
+import {
+  createTodoUpdateDtoMock,
+  createTodoMock,
+} from '../../../../../models/testing';
+import { fetchTodo, updateTodo } from '../../../../../store/todo';
+import { useTodoEditFacade } from './todo-edit.facade';
+
+const mockDispatch = jest.fn().mockResolvedValue({});
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+  useSelector: () => jest.fn(),
+}));
+
+jest.mock('../../../../../store/todo', () => ({
+  ...jest.requireActual('../../../../../store/todo'),
+  fetchTodo: jest.fn(),
+  updateTodo: jest.fn(),
+}));
+
+const mockStore = configureStore();
+const store = mockStore();
+
+const wrapper: FunctionComponent = ({ children }) => (
+  <MemoryRouter>
+    <Provider store={store}>{children}</Provider>
+  </MemoryRouter>
+);
+
+describe('useTodoEditFacade', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should dispatch fetchTodo', async () => {
+    const id = '1';
+    const { result } = renderHook(() => useTodoEditFacade({ id }), {
+      wrapper,
+    });
+    const { fetch } = result.current;
+    fetch({ id });
+
+    expect(mockDispatch).toHaveBeenCalled();
+    expect(fetchTodo).toHaveBeenCalledWith({ id });
+  });
+
+  it('should dispatch updateTodo', async () => {
+    const id = '1';
+    const { result } = renderHook(() => useTodoEditFacade({ id }), {
+      wrapper,
+    });
+    const { update } = result.current;
+    const dto = createTodoUpdateDtoMock();
+    const todo = createTodoMock();
+
+    mockDispatch.mockResolvedValue({ payload: { todo } });
+
+    update(id, dto);
+
+    expect(mockDispatch).toHaveBeenCalled();
+    expect(updateTodo).toHaveBeenCalledWith({ id, todo: dto });
+  });
+});
---
+++ src/pages/todo/todo-edit/containers/todo-edit/todo-edit.facade.ts
@@ -0,0 +1,50 @@
+import { AnyAction, ThunkDispatch, unwrapResult } from '@reduxjs/toolkit';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { TodoUpdateDto } from '../../../../../models';
+import {
+  fetchTodo,
+  updateTodo,
+  isFetchingSelector,
+  todoSelector,
+} from '../../../../../store/todo';
+
+export const useTodoEditFacade = (arg: { id: string }) => {
+  const { id } = arg;
+  const history = useHistory();
+  const dispatch = useDispatch<ThunkDispatch<unknown, undefined, AnyAction>>();
+  const isFetching = useSelector(isFetchingSelector);
+  const todo = useSelector(todoSelector);
+
+  const fetch = useCallback(
+    (arg: { id: string }) => {
+      return dispatch(fetchTodo(arg)).then(unwrapResult);
+    },
+    [dispatch]
+  );
+
+  const update = useCallback(
+    (id: string, dto: TodoUpdateDto) => {
+      return dispatch(updateTodo({ id, todo: dto }))
+        .then(unwrapResult)
+        .then((payload) => {
+          const { todo } = payload;
+          history.push(`/todos/${todo.id}`);
+        });
+    },
+    [history, dispatch]
+  );
+
+  useEffect(() => {
+    fetch({ id });
+  }, [id, fetch]);
+
+  return {
+    isFetching,
+    todo,
+    fetch,
+    update,
+  } as const;
+};
